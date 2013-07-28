b60af3ac1eefa0665b1be13c078d3535ec398dc9
Dean Sellis <dean@motoko.local>
2013-07-28 17:52:51 -0500

    Added new file for my git user

---
+++ modules/people/manifests/azumafuji.pp
@@ -0,0 +1,40 @@
+class people::azumafuji {
+  
+  # My OS X Options
+  include osx::global::disable_key_press_and_hold
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::dock::2d
+  include osx::dock::autohide
+  include osx::dock::clear_dock
+  include osx::finder::empty_trash_securely
+  include osx::finder::unhide_library
+  include osx::no_network_dsstores
+
+  include java
+  include clojure
+  include emacs
+  include virtualbox
+  include iterm2::dev
+  
+  include firefox
+  include chrome
+  include launchbar
+  include caffeine
+  include omnigraffle::pro
+
+
+  $home     = "/Users/dean"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  file { $dotfiles:
+    ensure => "directory",
+    mode => 700,
+  }
+
+  repository { $dotfiles:
+    source  => 'azumafuji/dotfile',
+  }
+}
